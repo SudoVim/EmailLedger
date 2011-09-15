291392913307772da6e46198ed6252bdc3365b90
Michael Renken <everything544@gmail.com>
2011-09-14 19:39:26 -0500

    Added a test for the User object

--- testing/LedgerTest.py
+++ testing/LedgerTest.py
@@ -5,6 +5,7 @@ import random
 
 from lib.ledger import Ledger
 from lib.ledger import Due
+from lib.ledger import User
 
 class LedgerTest(unittest.TestCase):
 
@@ -166,4 +167,31 @@ class AddDueTest(LedgerTest):
         self.assertEquals(msg, "The tables have turned! %s now owes %s $%s."
                                % (self.genUsername(1), self.genUsername(0),
                                   new_due.formatAmount()))
+
+class UserTest(unittest.TestCase):
+
+    TEST_UNAME = "uname"
+    TEST_EMAIL = "email"
+
+    def makeUser(self, usr_idx, eml_idx):
+        return User("%s%d" % (self.TEST_UNAME, usr_idx),
+                    "%s%d" % (self.TEST_EMAIL, eml_idx))
+
+    def test_UserEq(self):
+        uname_lista = [0, 0, 0, 0]
+        email_lista = [0, 0, 0, 0]
+
+        uname_listb = [0, 0, 1, 1]
+        email_listb = [0, 1, 0, 1]
+
+        eq_result = [True, False, False, False]
+        ne_result = [False, True, True, True]
+
+        for ua, ea, ub, eb, er, nr in zip(uname_lista, email_lista,
+            uname_listb, email_listb, eq_result, ne_result):
+            usr1 = self.makeUser(ua, ea)
+            usr2 = self.makeUser(ub, eb)
+
+            self.assertEquals(usr1 == usr2, er)
+            self.assertEquals(usr1 != usr2, nr)
 
